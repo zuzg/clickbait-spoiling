b13c0c49299342a7d6184dded2c657e53b89ab8a
Zuza Gawrysiak <gawrysiak.zuzanna@gmail.com>
2023-04-13 20:52:43 +0200

    Modify data reading

--- src/data.py
+++ src/data.py
@@ -2,17 +2,19 @@ import json
 import pandas as pd
 
 
-def jsonl_to_df(data_json: list) -> pd.DataFrame:
-    return pd.DataFrame(
+def read_data(filename: str) -> pd.DataFrame:
+    """
+    Read data into dataframe from provided filename
+
+    :param filename: name of the file to load
+    :return: dataframe with contents of the provided file
+    """
+    data_json = [json.loads(i) for i in open(filename, 'r')]
+    df = pd.DataFrame(
         [{'id': i['uuid'],
           'title': i['targetTitle'],
           'question': ' '.join(i['postText']),
           'context': i['targetTitle'] + ' - ' +
           (' '.join(i['targetParagraphs'])),
           'spoiler': i['spoiler']} for i in data_json])
-
-
-def read_data(filename: str) -> pd.DataFrame:
-    open_json = [json.loads(i) for i in open(filename, 'r')]
-    dataset = jsonl_to_df(open_json)
-    return dataset
+    return df
