b5e23c4929a6681ad46446b57e8a8c9ae9cced62
Zuza Gawrysiak <gawrysiak.zuzanna@gmail.com>
2023-04-16 19:28:53 +0200

    Add dataset class

--- src/data.py
+++ src/data.py
@@ -1,5 +1,9 @@
 import json
+import numpy as np
 import pandas as pd
+import torch
+from torch.utils.data import Dataset
+from transformers import AutoTokenizer
 
 
 def read_data(filename: str) -> pd.DataFrame:
@@ -18,3 +22,31 @@ def read_data(filename: str) -> pd.DataFrame:
           (' '.join(i['targetParagraphs'])),
           'spoiler': i['spoiler']} for i in data_json])
     return df
+
+
+class ClickbaitDataset(Dataset):
+    def __init__(self, df: pd.DataFrame, tokenizer_model: str) -> None:
+        super().__init__()
+        self.df = df
+        self.tokenizer = AutoTokenizer.from_pretrained(tokenizer_model)
+        self.encodings = self.tokenizer(
+            self.df.context, self.df.question, truncation=True, padding=True)
+        self.encodings.update({'encoding_ids': np.arange(len(df)),
+                               "contexts": df.context.tolist(),
+                               "questions": df.question.tolist()})
+        # optionally: add spoiler positions
+
+    def __len__(self) -> int:
+        return len(self.encodings.input_ids)
+
+    def __getitem__(self, idx: int) -> dict:
+        device = "cpu"
+        if torch.cuda.is_available():
+            device = "cuda"
+        batch = {}
+        for key, val in self.encodings.items():
+            if type(val[idx]) == str or isinstance(val[idx], tuple):
+                batch[key] = val[idx]
+            else:
+                batch[key] = torch.tensor(val[idx]).to(device)
+        return batch
