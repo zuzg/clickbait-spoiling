1fe411ed8573b63fe4b938e0cc55724a9b3a5e1f
azywot <agata.zywot@gmail.com>
2023-05-25 20:17:43 +0200

    create BERT classification pipeline

---
+++ src/model_pipelines/bert_classifier.py
@@ -0,0 +1,140 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import transformers
+from torch.utils.data import Dataset
+from tqdm import tqdm
+
+
+class BertDataset(Dataset):
+    """
+    Dataset class for Clickbait Spoiler Classification
+    """
+
+    def __init__(self, tokenizer, train_df, max_length=512):
+        """
+        :param tokenizer: BertTokenizer
+        :param train_df: Pandas DataFrame
+        :param max_length: Maximum length of the input sequence
+        """
+        super(BertDataset, self).__init__()
+        self.train_df = train_df
+        self.tokenizer = tokenizer
+        self.target = train_df["tags"]
+        self.max_length = max_length
+
+    def __len__(self):
+        return len(self.train_df)
+
+    def process_input(self, text, index):
+        """
+        :param text: Input text
+        :param index: Index of the text in the dataset
+        :return: Dictionary of input_ids, attention_mask, token_type_ids and target
+        """
+        inputs = self.tokenizer.encode_plus(
+            text,
+            None,
+            pad_to_max_length=True,
+            add_special_tokens=True,
+            return_attention_mask=True,
+            max_length=self.max_length,
+            truncation=True,
+        )
+        ids = inputs["input_ids"]
+        token_type_ids = inputs["token_type_ids"]
+        mask = inputs["attention_mask"]
+
+        return {
+            "ids": torch.tensor(ids, dtype=torch.long),
+            "mask": torch.tensor(mask, dtype=torch.long),
+            "token_type_ids": torch.tensor(token_type_ids, dtype=torch.long),
+            "target": torch.tensor(self.target[index], dtype=torch.long),
+        }
+
+    def __getitem__(self, index):
+        text = self.train_df.iloc[index]["text"]
+        return self.process_input(text, index)
+
+
+class BERTClassifier(nn.Module):
+    """
+    BERT Classifier for Clickbait Spoiler Classification
+    """
+
+    def __init__(self, model_checkpoint):
+        super(BERTClassifier, self).__init__()
+        self.bert_model = transformers.BertModel.from_pretrained(model_checkpoint)
+        self.dropout1 = nn.Dropout(0.1)  # regularization
+        self.fc1 = nn.Linear(768, 512)  # fully connected layer
+        self.dropout2 = nn.Dropout(0.1)  # regularization
+        self.fc2 = nn.Linear(512, 256)  # fully connected layer
+        self.relu = nn.ReLU()  # non-linearity
+        self.fc3 = nn.Linear(256, 1)  # output
+
+    def forward(self, ids, mask, token_type_ids):
+        _, x = self.bert_model(
+            ids, attention_mask=mask, token_type_ids=token_type_ids, return_dict=False
+        )
+        x = self.dropout1(x)
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.dropout2(x)
+        x = self.fc2(x)
+        x = self.relu(x)
+        out = self.fc3(x)
+        return out
+
+
+def finetune_BERT(epochs, dataloader, model, loss_fn, optimizer):
+    """
+    Finetune BERT model for Clickbait Spoiler Classification
+    :param epochs: Number of epochs to train
+    :param dataloader: DataLoader
+    :param model: BERTClassifier
+    :param loss_fn: Loss function
+    :param optimizer: Optimizer
+    :return: Finetuned BERT model
+    """
+    # freeze BERT parameters
+    for param in model.bert_model.parameters():
+        param.requires_grad = False
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.train()
+    model.to(device)
+
+    for epoch in range(epochs):
+        correct = 0
+        total = 0
+
+        loop = tqdm(enumerate(dataloader), total=len(dataloader))
+        for batch, dl in loop:
+            ids = dl["ids"].to(device)
+            token_type_ids = dl["token_type_ids"].to(device)
+            mask = dl["mask"].to(device)
+            label = dl["target"].to(device)
+            label = label.unsqueeze(1)
+
+            optimizer.zero_grad()
+
+            output = model(ids=ids, mask=mask, token_type_ids=token_type_ids)
+            label = label.type_as(output)
+
+            loss = loss_fn(output, label)
+            loss.backward()
+
+            optimizer.step()
+
+            pred = np.where(output.cpu() >= 0, 1, 0)
+
+            correct += sum(1 for a, b in zip(pred, label) if a[0] == b[0])
+            total += pred.shape[0]
+            accuracy = correct / total
+
+            loop.set_description(
+                f"Epoch={epoch}/{epochs} (accuracy: {correct}/{total})"
+            )
+            loop.set_postfix(loss=loss.item(), acc=accuracy)
+
+    return model
